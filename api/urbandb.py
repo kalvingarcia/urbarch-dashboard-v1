0210dbdb4341647f2ebc16bba05ed0e1b29c1417
Kalvin Garcia <kalvigarcia@gmail.com>
2024-01-24 16:40:11 -0500

    created a test environment to test the API

--- api/urbandb.py
+++ api/urbandb.py
@@ -63,3 +63,85 @@ class UrbanDB:
         except Exception as e:
             print(f"[{bold_bright_red}ERROR{default}  ] [Postgres     ] Could not update data from table ({table_name}) for object {id}.")
             print(f"[{bold_bright_red}ERROR{default}  ] [             ] {e}.")
+
+    def print_table(self, table_name):
+        try:
+            self.cursor.execute(f"SELECT * FROM {table_name}")
+            rows = self.cursor.fetchall()
+            for row in rows:
+                print('\t\t'.join(row))
+        except Exception as e:
+            print(f"[{bold_bright_red}ERROR{default}  ] [Postgres     ] Could not print data from table ({table_name}).")
+            print(f"[{bold_bright_red}ERROR{default}  ] [             ] {e}.")
+
+if __name__ == "__main__":
+    def menu():
+        print("1. ADD PRODUCTS")
+        print("2. DELETE PRODUCTS")
+        print("3. UPDATE PRODUCT")
+        print("4. RESET DATABASE")
+        print("5. PRINT DATABASE")
+        print("6. COMMIT TO DATABASE")
+        print("Q. QUIT")
+        return input("CHOICE: ")
+
+    def keep_going():
+        choice = input("Keep going? (Y / N): ")
+        if choice.upper() == 'Y':
+            return 0
+
+    def confirm():
+        choice = input("Are you sure? (Y / N): ")
+        if choice.upper() == 'Y':
+            return 0
+
+    def commit():
+        choice = input("Commit changes? (Y / N): ")
+        if choice.upper() == 'Y':
+            return 0
+
+    urb = UrbanDB("sessions/database-env.json")
+
+    quit = False
+    while not quit:
+        choice = menu()
+        if choice == '1':
+            while keep_going() is not None:
+                prod_json = {}
+
+                prod_json["id"] = input("Enter the product UA ID: ")
+                prod_json["name"] = input("Enter the product name: ")
+                prod_json["url"] = input("Enter the product URL: ")
+
+                urb.insert("small_search", prod_json)
+        elif choice == '2':
+            while keep_going() is not None:
+                id = input("Enter the product UA ID: ")
+                urb.delete("small_search", id)
+        elif choice == '3':
+            prod_json = {}
+
+            prod_json["id"] = input("Enter the product UA ID: ")
+            prod_json["name"] = input("Enter the new product name: ")
+            prod_json["url"] = input("Enter the new product URL: ")
+
+            urb.update("small_search", prod_json["id"], name=prod_json["name"], url=prod_json["url"])
+
+            keep_going()
+        elif choice == '4' and confirm() is not None:
+            urb.drop("small_search")
+            urb.create("small_search", id="TEXT UNIQUE", name="TEXT", url="TEXT")
+        elif choice == '5':
+            urb.print_table("small_search")
+            keep_going()
+        elif choice == '6' and confirm is not None:
+            urb.commit()
+        elif choice.upper() == 'Q':
+            if commit() is not None:
+                urb.commit()
+            urb.close()
+            quit = True
+        else:
+            print(print(f"[{bold_bright_red}ERROR{default}  ] [UrbanDB      ] Option invalid please try again."))
+
+        system("clear")
