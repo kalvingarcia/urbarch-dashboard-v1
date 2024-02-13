4940a72f3798a7cfd308826268725193a6155ab0
Kalvin Garcia <kalvigarcia@gmail.com>
2024-02-13 13:17:29 -0500

    completed the screen maker and created the app

--- api/screenmaker.py
+++ api/screenmaker.py
@@ -4,29 +4,41 @@ import inspect
 from importlib import import_module
 from kivymd.uix.screenmanager import MDScreenManager as ScreenManager
 
-def get_caller_loc():
-    # get the path leading to this call to find the pages directory consistently
-    caller_location = inspect.stack()[2].filename
-    return caller_location[: -(len(os.path.basename(caller_location)) + 1)]
+# This is how far back in the stack '_get_caller_location' must look
+# _get_caller_location() -> __init__() -> __new__() -> ScreenMaker()
+BACK_TRACE = 3
 
+# This is the ScreenMaker class API, where the Kivy Screens are created for the screen manager.
+# Ideally, the class finds the pages subdirectory in the same place as the caller's directory.
+# Then the ScreenMaker creates the screen manager and each page
 class ScreenMaker:
-    '''
-    '''
     def __init__(self):
         self.manager = ScreenManager()
 
         # looking for the pages directory
-        directory = get_caller_loc() + "/pages"
+        directory = self._get_caller_location() + "/pages"
         page_files =[name for name in os.listdir(directory) if os.path.isfile(os.path.join(directory, name))] # getting all the page files
 
         # creating the screens for the pages
         for page_file in page_files:
-            new_page = self.create_page(os.path.join(directory, page_file))
+            new_page = self._create_page(os.path.join(directory, page_file))
             if new_page is not None:
                 self.manager.add_widget(new_page) # only adding the pages that don't error
         self.manager.current = "home"
 
-    def create_page(self, page_file):
+    # This function is used to create the screen manager and return it
+    # since that is all this class does
+    def __new__(cls):
+        instance = super().__new__(cls)
+        instance.__init__()
+        return instance.manager
+
+    def _get_caller_location(self):
+        # get the path leading to this call to find the pages directory consistently
+        caller_location = inspect.stack()[BACK_TRACE].filename
+        return caller_location[: -(len(os.path.basename(caller_location)) + 1)]
+
+    def _create_page(self, page_file):
         page_path = page_file[: -len(os.path.basename(page_file))]
 
         # appending the path of the file to the system so we can import directly
@@ -40,6 +52,5 @@ class ScreenMaker:
             class_name = "".join([word.title() for word in page_module_name.split('-')])
             cls = getattr(page_module, class_name)
             return cls()
-        except Exception as e:
-            print(f"[{bold_bright_red}ERROR{default}  ] [            ] {e}")
-            print(f"[{bold_bright_red}ERROR{default}  ] [ScreenMaker ] The module {page_module_name} did not contain a proper page class.")
+        except Exception:
+            raise Exception()
--- main.py
+++ main.py
@@ -1,10 +1,32 @@
+from kivy.core.window import Window
 from kivymd.app import MDApp as App
+
 from api.screenmaker import ScreenMaker
+from api.urbandb import UrbanDB
 
 class UrbanDashApp(App):
+    def __init__(self, **kwargs):
+        super(UrbanDashApp, self).__init__(**kwargs)
+
+        # initializing the database connection
+        UrbanDB.open_pygres("./sessions/database-env.json")
+        UrbanDB.initialize_database()
+
+        # theming the dash
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Blue"
+
+        # binding the close function
+        Window.bind(on_request_close = self.on_request_close)
+        Window.size = (1600, 900)
+
     def build(self):
         self.title = "Urban Dash"
-        return ScreenMaker().manager
+        return ScreenMaker()
+
+    def on_request_close(self, *args):
+        UrbanDB.close_pygres()
+        self.stop()
 
 if __name__ == "__main__":
     UrbanDashApp().run()
