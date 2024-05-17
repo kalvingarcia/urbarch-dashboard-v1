1ab9d4c304cf2e9888ab52a130ea801c776cad58
Kalvin Garcia <kalvigarcia@gmail.com>
2024-05-17 14:47:58 -0400

    removed unnecessary comments

--- databaseinit.py
+++ databaseinit.py
@@ -3,7 +3,4 @@ from api.database import Database
 Database.connect("./sessions/database-env.json")
 Database.reset()
 Database.initialize()
-# Database._pygres.add("product_variations", ("featured", "BOOL"))
-# Database._complete_action()
-# print(Database._pygres.columns("product_variations"))
 Database.disconnect()
